7a2cc01234c8f4461d3e90cb164eacccae045ae3
AngeloBla <angeloblajic@gmail.com>
2023-11-16 14:40:41 +0100

    update
    mit login seite

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -40,33 +40,11 @@ const Navbar = () => {
                 Pricing
               </Link>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
+            <li className="nav-item">
+              {/* Verwende Link für interne Links */}
+              <Link className="nav-link active" aria-current="page" to="/login">
+                Login
+              </Link>
             </li>
           </ul>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/sign-in.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import Login from './components/login';
