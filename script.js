a6c5d8ee123304b1d05b7411651a5ce6dbce0438
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-11-22 11:03:04 +0100

    Update 22.11.2023
    zusammenführung der beiden html dateien
    übernommene script änderungen
    überarbeitete css

--- script.js
+++ script.js
@@ -50,64 +50,33 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 laufzeitContainer.style.display = "none";
                 laufleistungContainer.style.display = "none";
             }
-    
+        // ########### Funktion Zahlung ENDE ##########
 
-
-                // ########### Funktion Zahlung ENDE ##########
-
-                // ########### Funktion Auswahl Autofarbe verlinkung Bilder ###########
-                function loadCarImage() {
-                    var carColor = document.getElementById('carColor').value;
-                    var imagePath;
-                    switch (carColor) {
-                        case 'red':
-                            imagePath = './testbilder/aussenfarberot.png';
-                            break;
-                        case 'blue':
-                            imagePath = './testbilder/aussenfarbeblau.png';
-                            break;
-                        case 'yellow':
-                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_rundum/landrover_vbs.jpg';
-                            break;
-                        case 'green':
-                            imagePath = './testbilder/aussenfarbegruen.png';
-                            break;
-                        case 'black':
-                            imagePath = './testbilder/aussenfarbeschwarz.png';
-                            break;
-                        case 'white':
-                            imagePath = './testbilder/aussenfarbeweiss.png';
-                            break;
-                        default:
-                            imagePath = './testbilder/error.png';
-                    }
-                        document.getElementById('selectedImage').src = imagePath;
-                }
                 // ########### Funktion Auswahl karosserie verlinkung Bilder ###########
                 function loadCarImage() {
                     var bodyColor = document.getElementById('bodyColor').value;
                     var imagePath;
                     switch (bodyColor) {
                         case 'body-red':
-                            imagePath = './testbilder/aussenfarberot.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/karosserie_rot.jpg';
                             break;
                         case 'body-blue':
-                            imagePath = './testbilder/aussenfarbeblau.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/karosserie_blau.jpg';
                             break;
                         case 'body-yellow':
                             imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/karosserie.jpg';
                             break;
                         case 'body-green':
-                            imagePath = './testbilder/aussenfarbegruen.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/karosserie_gruen.jpg';
                             break;
                         case 'body-black':
-                            imagePath = './testbilder/aussenfarbeschwarz.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/karosserie_schwarz.jpg';
                             break;
                         case 'body-white':
-                            imagePath = './testbilder/aussenfarbeweiss.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/karosserie_wei%C3%9F.jpg';
                             break;
                         default:
-                            imagePath = './testbilder/error.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/error/error.png';
                     }
                         document.getElementById('selectedImage').src = imagePath;
                 }
@@ -117,25 +86,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     var imagePath;
                     switch (hoodColor) {
                         case 'white-hood':
-                            imagePath = './testbilder/motorhaubeweiss.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/motorhaube_wei%C3%9F.jpg';
                             break;
                         case 'blue-hood':
-                            imagePath = './testbilder/motorhaubeblau.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/motorhaube_blau.jpg';
                             break;
                         case 'green-hood':
-                            imagePath = './testbilder/motorhaubegruen.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/motorhaube_gruen.jpg';
                             break;
                         case 'red-hood':
-                            imagePath = './testbilder/motorhauberot.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/motorhaube_rot.jpg';
                             break;
                         case 'yellow-hood':
                             imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/motorhaube.jpg';
                             break;
                         case 'black-hood':
-                            imagePath = './testbilder/motorhaubeschwarz.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/motorhaube_schwarz.jpg';
                             break;
                         default:
-                            imagePath = 'error.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/error/error.png';
                     }
                     document.getElementById('selectedImage').src = imagePath;
                 }
@@ -165,7 +134,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                             imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/farben/dach_schwarz.jpg';
                             break;
                         default:
-                            imagePath = 'error.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/error/error.png';
                     }
                     document.getElementById('selectedImage').src = imagePath;
                 }
@@ -177,13 +146,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     var imagePath;
                     switch (roofrack) {
                         case 'roof-rack0':
-                            imagePath = './testbilder/dachtraegerkein.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/karosserie.jpg';
                             break;
                         case 'roof-rack1':
                             imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/dachtraeger.jpg';
                             break;
                         default:
-                            imagePath = 'error.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/error/error.png';
                     }
                     document.getElementById('selectedImage').src = imagePath;
                 }
@@ -195,7 +164,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     var imagePath;
                     switch (windowcolor) {
                         case 'window0':
-                            imagePath = './testbilder/scheiben0getoent.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/scheibe.jpg';
                             break;
                         case 'window20':
                             imagePath = './testbilder/scheiben20getoent.png';
@@ -207,36 +176,69 @@ document.addEventListener('DOMContentLoaded', (event) => {
                             imagePath = './testbilder/scheiben80getoent.png';
                             break;
                         default:
-                            imagePath = 'error.png';
+                            imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/error/error.png';
                     }
                     document.getElementById('selectedImage').src = imagePath;
                 }
+                //  ################ funktion Auswahl zusatz mit Bilder ##########
 
-                // ############ Reifen ############
+                function loadzusatzImage() {
+                    var checkboxes = document.querySelectorAll('.zusatz-option');
+                    var imageElement = document.getElementById('selectedImage');
+                    var neuesBild;
+                
+                    checkboxes.forEach(function(checkbox) {
+                        if (checkbox.checked) {
+                            switch (checkbox.value) {
+                                case 'hitch':
+                                    // Logik für 'Anhängevorrichtung'
+                                    break;
+                                case 'underbody-protection':
+                                    // Logik für 'Unterbodenschutz'
+                                    break;
+                                case 'mudguard':
+                                    neuesBild = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/spritzschutz.jpg';
+                                    break;
+                                default:
+                                    neuesBild = 'standardbild.jpg'; // Ein Standardbild, falls kein Fall zutrifft
+                            }
+                        }
+                    });
+                
+                    // Setze das neue Bild nur, wenn neuesBild definiert wurde
+                    if (neuesBild) {
+                        imageElement.src = neuesBild;
+                    }
+                }
+                
+                document.querySelectorAll('.zusatz-option').forEach(function(checkbox) {
+                    checkbox.addEventListener('change', loadzusatzImage);
+                });
+
                 // ############ Reifen ############
                 $(document).ready(function () {
                     var ganzjahresCheckbox = $('input[type="checkbox"][value="all-season-tire"]');
                     var sommerCheckbox = $('input[type="checkbox"][value="sommer"]');
                     var winterCheckbox = $('input[type="checkbox"][value="winter"]');
-                
+
                     // Setze Sommerreifen als Standard ausgewählt
                     sommerCheckbox.prop('checked', true);
-                
+
                     // Hinzufügen des Event Listeners für die Änderung der Checkboxen
-                    $('input[type="checkbox"]').change(function () {
+                    $('#reifen input[type="checkbox"]').change(function () {
                         updateOptions($(this));
                         updateReifenImage(true); // true signalisiert, dass die Funktion aufgrund einer Benutzeraktion aufgerufen wurde
                     });
-                
+
                     // Hinzufügen des Event Listeners für die Änderung der Ganzjahresreifen-Checkbox
                     ganzjahresCheckbox.change(function () {
                         updateGanzjahresCheckbox();
                     });
-                
+
                     // Funktion zum Aktualisieren der Checkboxen
                     function updateOptions(clickedCheckbox) {
                         var selectedValue = clickedCheckbox.val();
-                
+
                         if (selectedValue === 'all-season-tire') {
                             sommerCheckbox.prop('checked', false).prop('disabled', true);
                             winterCheckbox.prop('checked', false).prop('disabled', true);
@@ -247,7 +249,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                             }
                         }
                     }
-                
+
                     // Funktion zum Aktualisieren der Ganzjahresreifen-Checkbox
                     function updateGanzjahresCheckbox() {
                         if (!ganzjahresCheckbox.is(':checked')) {
@@ -255,11 +257,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
                             winterCheckbox.prop('disabled', false);
                         }
                     }
-                
+
                     // Funktion zur Aktualisierung des Reifenbildes
                     function updateReifenImage(userAction = false) {
                         if (!userAction) return; // Beendet die Funktion, wenn sie nicht durch Benutzeraktion aufgerufen wurde
-                
+
                         var imagePath;
                     if (ganzjahresCheckbox.is(':checked')) {
                         imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/reifen.jpg';
@@ -272,11 +274,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     } else {
                         imagePath = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/standart/reifen.jpg'; // Standardbild
                     }
-                
+
                     var imageElement = document.getElementById('selectedImage');
                     imageElement.src = imagePath;
                     imageElement.onerror = function() {
-                        this.src = 'error.png'; // Fallback-Bild
+                        this.src = 'https://lego-defender-model-auto.s3.eu-central-1.amazonaws.com/bilder/landrover_web_lagerteile/error/error.png'; // Fallback-Bild
                     };
                     }
 });
@@ -300,3 +302,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
             // ########## Javascript Preis abrufen ENDE ##########
+
+// Event Listener für zusätzliche Optionen
+$('.zusatz-option').change(function () {
+    // Ihre Logik für zusätzliche Optionen
+});
+
+// Event Listener für zusätzliche Optionen
+$('.service-option').change(function () {
+    // Ihre Logik für zusätzliche Optionen
+});
+
+// Event Listener für Service-Checkboxen
+$('#service input[type="checkbox"]').change(function () {
+    // Ihre Logik für Service-Optionen
+    // Zum Beispiel: Aktualisieren eines Preisanzeigefelds oder anderer Elemente basierend auf der Auswahl
+});
