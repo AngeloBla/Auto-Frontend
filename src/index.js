82c5a07f4976c7d49f898358f74c25958e894df5
Martin Krueger <122008999+Martin20231@users.noreply.github.com>
2023-11-23 11:42:53 +0100

    23.11.2023 Stand

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-// index.js
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+//import 'bootstrap/dist/css/sign-in.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Einloggen from "./components/kunden"; 
-import MyComponent from "./components/mitarbeiter_login";
-import "bootstrap/dist/css/bootstrap.min.css";
+import App from './components/App';
+import Login from './components/login';
+import Konfig from './components/konfig';
+import { Account } from './components/Accounts/Account';
+import Signup from './components/signup';
+import ChangePassword from './components/Accounts/ChangePassword';
 
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const rootElement = createRoot(root);
+
+rootElement.render(
   <Router>
     <Routes>
-      <Route path="/" element={<Einloggen />} />
-      <Route path="/kunden" element={<Einloggen />} />
-      <Route path="/mitarbeiter_login" element={<MyComponent />} />
+      <Route path="/" element={<App />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Account><Login /></Account>} />
+      <Route path="/changepassword" element={<Account><ChangePassword /></Account>} />
+      <Route path="/konfig" element={<Konfig />} />
     </Routes>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
