b728811dea57faa8f2f313a7c6e11426e6c014a4
AngeloBla <angeloblajic@gmail.com>
2023-11-24 13:52:25 +0100

    update album

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -56,8 +56,8 @@ class Album extends Component {
       <section className="py-5 text-center container">
         <div className="row py-lg-5">
           <div className="col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light">Album example</h1>
-            <p className="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+            <h1 className="fw-light">Lassen Sie sich inspirieren!</h1>
+            <p className="lead text-body-secondary">Alles was das Land Rover Herz benötigt</p>
           </div>
         </div>
       </section>
