69579f628861c94d482f8ec32f9f5f7bd323ecdd
phlepp1111 <phlpp.neumann@gmail.com>
2023-11-24 13:19:17 +0100

    erster Push

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/css/sign-in.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './components/App';
-import Login from './components/login';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "bootstrap/dist/css/sign-in.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./components/App";
+import Login from "./components/login";
+import Konfig from "./components/konfig";
 
-
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 const rootElement = createRoot(root);
 
 rootElement.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </Router>
+    <Router>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/konfig" element={<Konfig />} />
+        </Routes>
+    </Router>
 );
