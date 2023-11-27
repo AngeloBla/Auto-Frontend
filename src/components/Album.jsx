4884944e95103e233ab5ad48c2776f147ad16018
AngeloBla <angeloblajic@gmail.com>
2023-11-27 11:15:18 +0100

    aktuelle

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -56,8 +56,6 @@ class Album extends Component {
       <section className="py-5 text-center container">
         <div className="row py-lg-5">
           <div className="col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light">Lassen Sie sich inspirieren!</h1>
-            <p className="lead text-body-secondary">Alles was das Land Rover Herz benötigt</p>
           </div>
         </div>
       </section>
