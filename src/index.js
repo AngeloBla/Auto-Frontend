b978c5f5e572cc5631b4d00639384db17993e8b6
Martin Krueger <122008999+Martin20231@users.noreply.github.com>
2023-11-27 18:38:50 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,41 +1,30 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 //import 'bootstrap/dist/css/sign-in.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./components/App";
-import Login from "./components/login";
-import Konfig from "./components/konfig";
-import { Account } from "./components/Accounts/Account";
-import Signup from "./components/signup";
-import ChangePassword from "./components/Accounts/ChangePassword";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './components/App';
+import Login from './components/login';
+import Konfig from './components/konfig';
+import { Account } from './components/Accounts/Account';
+import Signup from './components/signup';
+import ChangePassword from './components/Accounts/ChangePassword';
+import VerifyEmail from './components/verifyEmail';
 
-const root = document.getElementById("root");
+
+const root = document.getElementById('root');
 const rootElement = createRoot(root);
 
 rootElement.render(
-    <Router>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route
-                path="/login"
-                element={
-                    <Account>
-                        <Login />
-                    </Account>
-                }
-            />
-            <Route
-                path="/changepassword"
-                element={
-                    <Account>
-                        <ChangePassword />
-                    </Account>
-                }
-            />
-            <Route path="/konfig" element={<Konfig />} />
-        </Routes>
-    </Router>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/verifyEmail" element={<VerifyEmail />} />
+      <Route path="/login" element={<Account><Login /></Account>} />
+      <Route path="/changepassword" element={<Account><ChangePassword /></Account>} />
+      <Route path="/konfig" element={<Konfig />} />
+    </Routes>
+  </Router>
 );
