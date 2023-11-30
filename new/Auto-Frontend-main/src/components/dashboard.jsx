ab87cdd936de8fe4195c901dab198dd702b68003
Martin Krueger <122008999+Martin20231@users.noreply.github.com>
2023-11-30 15:03:39 +0100

    Update 30.11.2023 dashboard.jsx

--- new/Auto-Frontend-main/src/components/dashboard.jsx
+++ new/Auto-Frontend-main/src/components/dashboard.jsx
@@ -4,18 +4,32 @@ import { Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
 
 const Dashboard = () => {
-  // Beispiel-Daten für den Barchart
-  const chartData = {
+  const einnahmenData = {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
     datasets: [
       {
-        label: 'Number of Sales',
+        label: 'Einnahmen pro Monat',
         backgroundColor: 'rgba(75,192,192,0.2)',
         borderColor: 'rgba(75,192,192,1)',
         borderWidth: 1,
         hoverBackgroundColor: 'rgba(75,192,192,0.4)',
         hoverBorderColor: 'rgba(75,192,192,1)',
-        data: [65, 59, 80, 81, 56],
+        data: [6302535, 6545275, 6995275, 6302535, 6999775, 7499775, 7999775, 7995775, 8999775, 9499775, 9999775, 10499775],
+      },
+    ],
+  };
+
+  const ausgabenData = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    datasets: [
+      {
+        label: 'Ausgaben pro Monat',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [4002535, 4545275, 4095275, 4302535, 4599775, 5199775, 5999775, 5195775, 6199775, 7199775, 7599775, 8199775],
       },
     ],
   };
@@ -24,7 +38,7 @@ const Dashboard = () => {
     scales: {
       x: {
         type: 'category',
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       },
       y: {
         beginAtZero: true,
@@ -32,14 +46,13 @@ const Dashboard = () => {
     },
     maintainAspectRatio: false,
     responsive: true,
-};
+  };
 
   return (
     <div>
       <Navbar />
       {/* Hinzugefügte Linie */}
-      <div style={{ borderBottom: '3px solid gray' }}>
-      </div>
+      <div style={{ borderBottom: '3px solid gray' }}></div>
 
       <div className="container-fluid">
         <div className="row">
@@ -56,17 +69,12 @@ const Dashboard = () => {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="/dashboard">
-                  Dashboard
+                    Dashboard
                   </a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="/lager">
                     Lager
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/konsturktion">
-                    Konsturktion
                   </a>
                 </li>
                 {/* Weitere Navigationspunkte hier hinzufügen */}
@@ -76,18 +84,18 @@ const Dashboard = () => {
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            
-
             </div>
 
-           
-            {/* Barchart */}
-            <div className="mt-5" style={{ height: '500px', width: '500px' }}>
-            <h2 style={{ textAlign: 'center' }}>Anzahl der Verkäufe</h2>
-            <Bar data={chartData} options={chartOptions} />
-
-
-
+            {/* Barcharts */}
+            <div className="mt-5" style={{ display: 'flex', justifyContent: 'space-around' }}>
+              <div style={{ height: '600px', width: '600px' }}>
+                <h2 style={{ textAlign: 'center' }}>Einnahmen Astley Motors</h2>
+                <Bar data={einnahmenData} options={chartOptions} />
+              </div>
+              <div style={{ height: '600px', width: '600px' }}>
+                <h2 style={{ textAlign: 'center' }}>Ausgaben Astley Motors</h2>
+                <Bar data={ausgabenData} options={chartOptions} />
+              </div>
             </div>
           </main>
         </div>
