69be34cfb4c56e4eb0e7ff5155bf2ad5c6d149f4
= <122011721+lindnech@users.noreply.github.com>
2023-12-04 00:07:34 +0100

    update

--- src/components/Accounts/Userpool.js
+++ src/components/Accounts/Userpool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "eu-central-1_Egx5xezvi",
-    ClientId: "3bokkt3i0b6k2iu8scf15s50qs"
+    UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+    ClientId: process.env.REACT_APP_CLIENT_ID
 }
 
 const UserPool = new CognitoUserPool(poolData);
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-//import 'bootstrap/dist/css/sign-in.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 import Login from './components/login';
 import Konfig from './components/konfig';
 import { Account } from './components/Accounts/Account';
+import MitarbeiterDashboard from './components/dashboard';
 import Signup from './components/signup';
 import ChangePassword from './components/Accounts/ChangePassword';
 import VerifyEmail from './components/verifyEmail';
+import Lager from './components/lager';
+import Konstruktion from './components/konsturktion';
+
 
 
 const root = document.getElementById('root');
@@ -25,6 +28,9 @@ rootElement.render(
       <Route path="/login" element={<Account><Login /></Account>} />
       <Route path="/changepassword" element={<Account><ChangePassword /></Account>} />
       <Route path="/konfig" element={<Konfig />} />
+      <Route path="/dashboard" element={<MitarbeiterDashboard />} />
+      <Route path="/lager" element={<Lager />} />
+      <Route path="/konsturktion" element={<Konstruktion />} />
     </Routes>
   </Router>
 );
